b15d38c02288a5cd17069943d58d0d1e0487fdfe
Dominique Blomsma <49249239+DominiqueBlomsma@users.noreply.github.com>
2023-09-25 20:52:00 +0200

    Added account_namespace request (#2)

---
+++ src/XRPL.NET/XRPL.NET/Models/Methods/Accounts/AccountNamespaceRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace XRPL.NET.Models.Methods.Accounts;
+
+public class AccountNamespaceRequest : LedgerRequestBase, IPaginationMarker
+{
+    /// <summary>
+    /// A unique identifier for the account, most commonly the account's <see href="https://xrpl.org/basic-data-types.html#addresses">xAddress</see>.
+    /// </summary>
+    [Required]
+    [JsonPropertyName("account")]
+    public string Account { get; set; } = default!;
+
+    [JsonPropertyName("namespace_id")]
+    public string NamespaceId { get; set; } = default!;
+
+    /// <summary>
+    /// The maximum number of objects to include in the results.
+    /// Must be within the inclusive range 10 to 400 on non-admin connections. The default is 200.
+    /// </summary>
+    [JsonPropertyName("limit")]
+    public int? Limit { get; set; }
+
+    /// <summary>
+    /// Value from a previous paginated response.
+    /// Resume retrieving data where that response left off.
+    /// </summary>
+    [JsonPropertyName("marker")]
+    public object? Marker { get; set; }
+}
