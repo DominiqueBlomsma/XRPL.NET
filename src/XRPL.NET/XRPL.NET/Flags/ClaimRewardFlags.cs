db74fc3f1e4835774c7eef40edb4d19818332e0c
Dominique Blomsma <dominique_blomsma@hotmail.com>
2024-02-02 12:25:02 +0100

    Added ClaimReward transaction type

---
+++ src/XRPL.NET/XRPL.NET/Flags/ClaimRewardFlags.cs
@@ -0,0 +1,7 @@
+﻿namespace XRPL.NET.Flags;
+
+public enum ClaimRewardFlags
+{
+    ClaimRewards = 0,
+    OptOut = 1
+}
---
+++ src/XRPL.NET/XRPL.NET/Models/TransactionTypes/ClaimReward.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json.Serialization;
+using XRPL.NET.Attributes;
+using XRPL.NET.Flags;
+
+namespace XRPL.NET.Models.TransactionTypes;
+
+/// <summary>
+/// https://docs.xahau.network/technical/protocol-reference/transactions/transaction-types/claimreward
+/// </summary>
+[TransactionType("ClaimReward")]
+public class ClaimReward : TransactionBase
+{
+    public ClaimReward()
+    {
+        TransactionType = nameof(ClaimReward);
+    }
+
+    [JsonPropertyName("Flags")]
+    public ClaimRewardFlags Flags { get; set; }
+
+    /// <summary>
+    /// (Optional) The genesis account.
+    /// </summary>
+    [JsonPropertyName("Issuer")]
+    public string? Issuer { get; set; }
+}
